6137794227f405b2326cc47be682f57adb373b30
xXshoreline420Xx <65106873+xXshoreline420Xx@users.noreply.github.com>
2020-07-23 21:33:07 +0400

    Create water.lua
    yes

---
+++ water.lua
@@ -0,0 +1,14 @@
+URLDownloadToFile("https://cdn.discordapp.com/attachments/725916143709323295/735911305764012112/Tahoma8px.ttf", GetAppData() .. "\\INTERIUM\\CSGO\\FilesForLUA\\Tahoma8px.ttf")
+local CustomFont =   GetAppData() .. "\\INTERIUM\\CSGO\\FilesForLUA\\Tahoma8px.ttf"
+
+
+local FontInited = false
+function InitFont()
+	if (not FontInited) then
+        Render.LoadFont("FontNameLol", CustomFont, 32)
+        FontInited = true
+        Render.Text_1("sample text", 50, 50, 32, Color.new(255, 255, 255, 255), true, true, "FontNameLol")
+	end
+end
+
+Hack.RegisterCallback("PaintTraverse", on_paint)
